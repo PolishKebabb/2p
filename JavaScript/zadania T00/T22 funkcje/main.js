3d8c6bf1ffcd6d3d82f66873a7ae9b00de733c38
2p02 <oskar200307@gmail.com>
2024-01-02 14:17:11 +0100

    t22

--- JavaScript/zadania T00/T22 funkcje/main.js
+++ JavaScript/zadania T00/T22 funkcje/main.js
@@ -7,5 +7,13 @@ let y = 3;
 let wartoscBezwzgledna1 = bezwzgledna1(a);
 let wartoscBezwzgledna2 = bezwzgledna2(a);
 
-wynik.innerHTML = `wartość bezwgledna z ${a} = ${wartoscBezwzgledna1} (pierwszy sposob)
- <br> |a| = ${wartoscBezwzgledna2} (drugi sposob)`
+let potega = doPotegi(x,y);
+
+wynik.innerHTML = `Zadanie t22_1 <br><br>
+wartość bezwgledna z ${a} = ${wartoscBezwzgledna1} (pierwszy sposob)
+ <br> |a| = ${wartoscBezwzgledna2} (drugi sposob) <br><br>
+
+Zadanie t22_2 <br><br>
+x = ${x} <br>
+y = ${y} <br>
+`
