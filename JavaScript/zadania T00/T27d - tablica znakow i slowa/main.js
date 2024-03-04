c635e22d84c66cbf3ea580f750f9ef6e99fef903
2p02 <oskar200307@gmail.com>
2024-03-04 15:06:08 +0100

    eds

--- JavaScript/zadania T00/T27d - tablica znakow i slowa/main.js
+++ JavaScript/zadania T00/T27d - tablica znakow i slowa/main.js
@@ -1,13 +1,13 @@
 const btn = document.querySelector('button');
-let dlugosc = document.getElementById('dlugosc');
-dlugosc = dlugosc.value;
 let znaki = [];
 btn.addEventListener('click', function() {
     let znakiInput = document.getElementById('znaki');
     let lancuch = znakiInput.value;
+    let dloogosc = document.getElementById('dlugosc');
+    let dloogosc = dloogosc.value;
 
     znaki = lancuch.split('');
 
     console.log(znaki);
-    console.log(dlugosc)
+    console.log(dloogosc);
 });
