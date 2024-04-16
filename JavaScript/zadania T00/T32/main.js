915585bec5465360e857707e40bcfaba1d30d303
2p02 <oskar200307@gmail.com>
2024-04-16 14:57:45 +0200

    a

--- JavaScript/zadania T00/T32/main.js
+++ JavaScript/zadania T00/T32/main.js
@@ -3,8 +3,17 @@ const btn = document.querySelector("button");
 btn.addEventListener('click', function (){
     let ocena = document.querySelector("#ocena").value;
     let kolor = document.querySelector("#kolor").value;
+    let data = document.querySelector("#data").value;
 
     let naglowek = document.querySelector("h1");
     naglowek.style.color = kolor;
+
+    let ocena2 = document.querySelector("#ocena2");
+    ocena2.innerHTML = `${ocena}`;
+    let kolor2 = document.querySelector("#kolor2");
+    kolor2.style.color = kolor;
+
+    let data2 = document.querySelector("#data2");
+    data2.innerHTML = `${data}`
 
 });
