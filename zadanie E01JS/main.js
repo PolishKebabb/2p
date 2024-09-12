eb1be2b40cef1ada9ea4dcd191ea38b16cf6a44e
PolishKebabb <oskar200307@gmail.com>
2024-09-12 09:24:35 +0200

    a

--- zadanie E01JS/main.js
+++ zadanie E01JS/main.js
@@ -3,13 +3,18 @@ const ilelitrów = document.querySelector(`#ilelitrów`)
 const wynik = document.querySelector(`#wynik`);
 const btn = document.querySelector(`button`);
 
-btn.addEventListener("click", function (){
+btn.addEventListener('click', function (){
     let rodz = parseInt(rodzaj.value);
     let ile = parseInt(ilelitrów.value)
     let koszt = 0;
 
-    if(rodz===1) koszt = ile * 4;
-    if(rodz===2) koszt = ile * 3.5;
-
-    wynik.innerHTML = `koszt paliwa: ${koszt} zł`;
+    if ((rodz === 1 || rodz === 2) && ile >= 0) {
+        if (rodz === 1)
+            koszt = ile * 4;
+        else if (rodz === 2)
+            koszt = ile * 3.5;
+        wynik.innerHTML = `koszt paliwa: ${koszt} zł`;
+    }
+    else
+        wynik.innerHTML = `wybrano złe wartości`;
 })
